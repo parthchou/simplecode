23f0f12ca178249615a3e319ee0c6054833461d8
Arya <132542202+Aa9224487402@users.noreply.github.com>
2023-09-03 08:37:53 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class simple {
 public static void main(String args[]){
-System.out.println("Hello Batch32 Team with Arya");
+System.out.println("Hello Batch32 Team with Rockstar & I m the greater one");
 }
 }
