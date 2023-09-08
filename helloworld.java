22a1ab7b0c717bbc87e6782f898a3a06e4793ee0
Arya <132542202+Aa9224487402@users.noreply.github.com>
2023-09-08 16:30:30 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class simple {
 public static void main(String args[])
   {
-System.out.println("Hello Batch32 Team with Self confiden & Everytime do hardwork it will get success");
+System.out.println("Hello Batch32 Team with Om namah shivaya & Everytime do hardwork it will get success");
 }
 }
