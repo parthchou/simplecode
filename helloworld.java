cbb2b4da2bc70ec2f37fa422493ffe3dc6b46528
Arya <132542202+Aa9224487402@users.noreply.github.com>
2023-09-08 16:18:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 class simple {
-public static void main(String args[]){
-System.out.println("Hello Batch32 Team with Rockstar & I m the greater one");
+public static void main(String args[])
+  {
+System.out.println("Hello Batch32 Team with Rockstar & Everytime do hardwork it will get success");
 }
 }
