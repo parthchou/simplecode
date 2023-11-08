ec148ecdbb6de6fa20a7fcd0645ff45ea0086794
S R Arya DevOps <132542202+Aa9224487402@users.noreply.github.com>
2023-11-08 09:24:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class simple {
 public static void main(String args[])
   {
-System.out.println("Hello Parth Team with Om namah shivaya & Everytime do hardwork it will get success forever CI");
+System.out.println("Hello Parth well train in jenkins");
 }
 }
